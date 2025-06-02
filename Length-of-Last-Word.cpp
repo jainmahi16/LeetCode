3a061db6d404fe824865c67f0e5748ddd9cf8ab0
jainmahi16 <138456561+jainmahi16@users.noreply.github.com>
2025-06-02 19:15:06 +0530

    [58] [Time Beats: 100.00%] [Memory Beats: 66.11%] - LeetPush

--- Length-of-Last-Word.cpp
+++ Length-of-Last-Word.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    int lengthOfLastWord(string s) {
-        int cnt = 0;
-        int flag = 0;
-        for(int i = s.length() - 1; i >= 0; i--) {
-            if(flag == 0 && s[i] == ' ') {
-                continue;
-            }
-            if(s[i] != ' ') {
-                flag = 1;
-                cnt++;
-            } else {
-                break;
-            }
+    int lengthOfLastWord(const std::string& s) {
+        // Trim trailing spaces
+        int end = s.length() - 1;
+        while (end >= 0 && s[end] == ' ') {
+            end--;
         }
-        return cnt;
+        // Find the start of the last word
+        int start = end;
+        while (start >= 0 && s[start] != ' ') {
+            start--;
+        }
+        // The length of the last word is (end - start)
+        return end - start;
     }
 };
